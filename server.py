e0734f66069541764fb894a0816ca359306ee825
Valerie Yuan <jiayuyua@andrew.cmu.edu>
2024-02-15 20:14:50 -0500

    Finished the predict model and updated the routing.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import numpy as np
 import joblib
+import json
 
 app = Flask(__name__)
 
@@ -8,19 +9,34 @@ from flask import request
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    # Get the input array from the request
-    get_json = request.get_json()
-    iris_input = get_json['input']
+    # Example of handling a GET request with query parameters
+    input_values = request.args.get('input')
+    if input_values:
+        try:
+            iris_input = [float(x) for x in input_values.split(',')]
+            input_array = np.array(iris_input).reshape(1, -1)
+            model = joblib.load('iris_model.pkl')
+            prediction = model.predict(input_array)
+            # Manually serialize data to JSON and set Content-Type
+            response_body = json.dumps({'prediction': prediction.tolist()})
+            return response_body, 200, {'Content-Type': 'application/json'}
+        except Exception as e:
+            return json.dumps({'error': str(e)}), 400, {'Content-Type': 'application/json'}
+    else:
+        return json.dumps({'error': 'No input provided'}), 400, {'Content-Type': 'application/json'}
+    # # Get the input array from the request
+    # get_json = request.get_json()
+    # iris_input = get_json['input']
     
-    # TODO: Import trained model
-    model = ...
+    # # TODO: Import trained model
+    # model = joblib.load('iris_model.pkl')
     
-    # TODO: Make prediction using the model 
-    # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
+    # # TODO: Make prediction using the model 
+    # # HINT: use np.array().reshape(1, -1) to convert input to 2D array
+    # input_array = np.array(iris_input).reshape(1,-1)
+    # prediction = model.predict(input_array)
     
-    # TODO: Return the prediction as a response
-    return ...
+    # # TODO: Return the prediction as a response
 
 @app.route('/')
 def hello():
